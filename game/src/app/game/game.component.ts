44b8ef475a58f0dd98ef244ee1678692d184f808
Dmirty Anansky <Dmitry.Anansky@dataart.com>
2018-10-01 19:19:45 +0300

    Update game component with layout

--- game/src/app/game/game.component.ts
+++ game/src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  public titleTopnumbers: number[];
+  public titleLeftAlphabet: string[];
+  public consoleText: string = 'Please place your ships on the game battle field.\n' +
+    ' The Game will start immediately after all ship\'s positioning.';
+  public displayRotationControl: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.titleLeftAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+    this.titleTopnumbers = Array.from(Array(11).keys());
   }
 
+  getGridNumber(num: number) {
+    return Array.from(Array(num).keys());
+  }
+
+  onExitClick() {
+    this.router.navigate(['/intro']);
+  }
+
+  onPlaceShips() {
+    this.displayRotationControl = true;
+    this.consoleText = 'Use the mouse to place your ships on the battle field.';
+  }
 }
