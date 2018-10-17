24751a0a3204c903af318d3c6a6f95c28d9f7839
Dmirty Anansky <Dmitry.Anansky@dataart.com>
2018-10-17 15:56:13 +0300

    Start working on Game component tests

--- game/src/app/game/game.component.spec.ts
+++ game/src/app/game/game.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import { GameComponent } from './game.component';
 import {BotGridComponent} from './bot-grid/bot-grid.component';
@@ -12,7 +12,6 @@ import {GridService} from '../services/grid.service';
 
 describe('GameComponent', () => {
   let component: GameComponent;
-  let fixture: ComponentFixture<GameComponent>;
   const mockRouter = {
     navigate: jasmine.createSpy('navigate')
   };
@@ -26,12 +25,103 @@ describe('GameComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GameComponent);
+    const fixture = TestBed.createComponent(GameComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create GameComponent', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display app-game-information component', async(() => {
+    const fixture = TestBed.createComponent(GameComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-game-information')).not.toEqual(null);
+  }));
+
+  it('should display start text inside app-game-information component', async(() => {
+    const fixture = TestBed.createComponent(GameComponent);
+    const startGameText = 'Please place your ships on the game battle field.';
+
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-game-information .game-progress-log').textContent).toContain(startGameText);
+  }));
+
+  it('should display app-bot-grid component', async(() => {
+    const fixture = TestBed.createComponent(GameComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-bot-grid')).not.toEqual(null);
+  }));
+
+  it('should display app-player-grid component', async(() => {
+    const fixture = TestBed.createComponent(GameComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-player-grid')).not.toEqual(null);
+  }));
+
+  it('should display app-control-panel component', async(() => {
+    const fixture = TestBed.createComponent(GameComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-control-panel')).not.toEqual(null);
+  }));
+
+  it('should contain bot grid with title-top', async(() => {
+    const fixture = TestBed.createComponent(GameComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-bot-grid .title-top')).not.toEqual(null);
+  }));
+
+  it('should contain bot grid with title-left', async(() => {
+    const fixture = TestBed.createComponent(GameComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-bot-grid .title-left')).not.toEqual(null);
+  }));
+
+  it('should contain player grid with title-top', async(() => {
+    const fixture = TestBed.createComponent(GameComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-player-grid .title-top')).not.toEqual(null);
+  }));
+
+  it('should contain player grid with title-left', async(() => {
+    const fixture = TestBed.createComponent(GameComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-player-grid .title-left')).not.toEqual(null);
+  }));
 });
